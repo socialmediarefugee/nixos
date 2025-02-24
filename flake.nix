65187df00c079906ef26f6a6e4741c5d356ea6b3
Nunya bidness <1012004+socialmediarefugee@users.noreply.github.com>
2025-02-23 19:08:53 -0600

    Work on Users

--- flake.nix
+++ flake.nix
@@ -3,18 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-#    home-manager.url = "github:nix-community/home-manager";
-#    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-#    plasma-manager.url = "github:nix-community/plasma-manager";
-#    plasma-manager.inputs.nixpkgs.follows = "nixpkgs";
-#    plasma-manager.inputs.home-manager.follows = "home-manager";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixos-hardware = {
+      #url = "git+file:./nixos-hardware";
+      url = "github:/socialmediarefugee/nixos-hardware";
+      flake = false;
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs =
     inputs@{
       nixpkgs,
-#      home-manager,
-#      plasma-manager,
+      home-manager,
+      plasma-manager,
+      hyprland,
       ...
     }:
     {
@@ -23,8 +37,71 @@
           system = "x86_64-linux";
           modules = [
             ./configuration.nix
-	  ];
-        };
+	          ./modules/virtualisation.nix
+      	    ./modules/services.nix
+            {
+
+              users.users.root = {
+                packages = with nixpkgs.lib; [
+                  nixpkgs.legacyPackages.x86_64-linux.tcpdump
+                  nixpkgs.legacyPackages.x86_64-linux.nixos-option
+                  nixpkgs.legacyPackages.x86_64-linux.strace
+                ];
+              };
+
+              users.users.sunshine = {
+                isNormalUser = true;
+               description = "Nunya Bidness";
+               extraGroups = [
+                 "networkmanager"
+                 "wheel"
+                 "video"
+                 "libvirtd"
+                ];
+             };
+
+             users.users.apollo = {
+               isNormalUser = true;
+               home = "home/apollo";
+               description = "Apollo Hyprland";
+               extraGroups = [ 
+                 "networkmanager" 
+                 "wheel" 
+                 "video" 
+                 "libvirtd" 
+               ];
+             };
+
+             users.users.boomer = {
+               isNormalUser = true;
+               home = "/home/boomer";
+               description = "Boomer (Terminal)";
+               extraGroups = [ "networkmanager"];
+               shell = nixpkgs.lib.getExe nixpkgs.legacyPackages.x86_64-linux.bash;
+             };
+            }
+          {
+          environment.systemPackages = with nixpkgs.lib; [
+            nixpkgs.legacyPackages.x86_64-linux.nano
+          ];
+          programs.hyprland = {
+            enable = true;
+            packages = hyprland.packages.x86_64-linux.hyprland;
+          };
+          home-manager.modules.home-manager = {
+            home-manager.users.root = {pkgs,...}: {
+
+            };
+            home-manager.users.boomer = { pkgs, ...}: {
+              programs.bash.enable = true;
+            };
+            home-manager.users.apollo = {pkgs, ...}: {
+              wayland.windowManager.hyperland.enable = true;
+            };
+          };
+          }
+          ];
+        } # pikon
       };
     };
 }
